3775fe2596ba1b60647fd504d845720b59a277fa
Jayon Kim <jpkim921@gmail.com>
2018-09-10 00:54:56 -0400

    added validations to parent model

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,6 +2,10 @@ class Parent < ActiveRecord::Base
   has_many :children
   has_many :classroom, through: :children
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, uniqueness: true
+
 
 
 end
