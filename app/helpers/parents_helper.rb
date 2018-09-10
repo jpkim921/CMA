5607ad260500c0f0e07d984ecfda0013600c0250
Jayon Kim <jpkim921@gmail.com>
2018-09-10 00:55:39 -0400

    modified phone_number helper based on input

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,9 +1,12 @@
 module ParentsHelper
 
   def phone_number(parent)
-    area_code = parent.phone_number[0..2]
-    first_three = parent.phone_number[3..5]
-    last_four = parent.phone_number[6..-1]
+    phone_number = parent.phone_number.gsub!('-','')
+
+    area_code = phone_number[0..2]
+    first_three = phone_number[3..5]
+    last_four = phone_number[6..-1]
+    
     "(#{area_code}) #{first_three} - #{last_four}"
   end
 end
