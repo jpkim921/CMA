555fe2e5b6ef3ac229b01c0b4263012b538f5028
jpkim921 <jpkim921@gmail.com>
2018-09-13 13:31:37 -0400

    added classrooms#new and #create

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,4 +1,6 @@
 class ClassroomsController < ApplicationController
+  before_action :set_classroom, only: [:show, :edit, :update, :destroy]
+  
   def index
     @classrooms = Classroom.all
   end
@@ -7,5 +9,36 @@ class ClassroomsController < ApplicationController
     @classroom = Classroom.find(params[:id])
   end
   
-    
+  def new
+    @classroom = Classroom.new
+  end
+  
+  def create
+    @classroom = Classroom.create(classroom_params)
+
+    if @classroom.save
+      redirect_to classrooms_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update    
+  end
+  
+  
+  private
+  
+  def set_classroom
+    @classroom = Classroom.find(params[:id])
+  end
+  
+  def classroom_params
+    params.require(:classroom).permit(:name, :teacher_name, :age_low, :age_high)
+  end
+  
+  
 end
