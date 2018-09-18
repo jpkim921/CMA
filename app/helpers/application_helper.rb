f49107345d1408a97802e6c2f5b8dfc5f5f76dfd
jpkim921 <jpkim921@gmail.com>
2018-09-18 12:40:11 -0400

    added validation error messages for create parent, child and classroom forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def error_messages(object)
+    render(:partial => 'application/errors', :locals => {:object => object})
+  end
+  
   def name(person)
     person.first_name + " " + person.last_name
   end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,6 +2,10 @@ class Classroom < ActiveRecord::Base
   has_many :children
   has_many :parents, through: :children
   
+  validates :name, presence: true
+  validates :teacher_name, presence: true
+  validates :age_low, presence: true
+  validates :age_high, presence: true
   
   
   def age_range
