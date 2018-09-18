a7239af51e01e5260fae443d315c523e587425f5
Jayon Kim <jpkim921@gmail.com>
2018-09-17 22:19:33 -0400

    display child's classroom and teacher, even if classroom isn't assigned.

--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -4,4 +4,20 @@ module ChildrenHelper
     dob.strftime("%B %e, %Y")
   end
 
+  def display_classroom_name(child)
+    if child.classroom
+      child.classroom.name
+    else
+      "Classroom not assigned."
+    end
+  end
+
+  def display_teacher_name(child)
+    if child.classroom
+      child.teacher_name
+    else
+      "N/A"
+    end
+  end
+
 end
--- app/models/child.rb
+++ app/models/child.rb
@@ -4,13 +4,13 @@ class Child < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
-  
-  
+
+
+
   def age
     ((Time.zone.now - self.dob.to_time) / 1.year.seconds).floor
   end
-    
+
   def assign_classroom
     Classroom.all.each do |classroom|
 #       the class size is set to 3 for demonstration
@@ -18,5 +18,7 @@ class Child < ActiveRecord::Base
         self.classroom = classroom
       end
     end
-  end  
+  end
+
+
 end
