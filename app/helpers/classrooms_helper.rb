67ba81dfec7f027d8209fee4ceb64a631c4f40ba
Jayon Kim <jpkim921@gmail.com>
2018-09-21 16:56:33 -0400

    back to either classroom index or parent show depending on who's logged in.

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -1,7 +1,15 @@
 module ClassroomsHelper
-  
+
   def age_range(classroom)
-    "#{classroom.age_low} to #{classroom.age_high} year olds" 
+    "#{classroom.age_low} to #{classroom.age_high} year olds"
   end
-  
+
+  def back_to_homepage
+    if session[:admin]
+      classrooms_path
+    else
+      parent_path(session[:parent_id])
+    end
+  end
+
 end
