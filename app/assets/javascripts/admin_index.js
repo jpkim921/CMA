0882da4a1dfcdc4b10ed20b413cf3359bc2a320e
Jayon Kim <jpkim921@gmail.com>
2019-04-15 15:41:02 -0400

    clean up admin_index.js

--- app/assets/javascripts/admin_index.js
+++ app/assets/javascripts/admin_index.js
@@ -15,18 +15,14 @@ $(document).ready(function() {
 
       if (query === 'classrooms') {
         $.get(url, classroomsResults)
-
       } else if (query === 'parents') {
-        // console.log('PARENTS INDEX')
         $.get(url, parentsResults)
       } else if (query === 'children') {
         $.get(url, childrenResults)
-        // console.log('CHILDREN INDEX')
       }
 
     }
   );
-  // sortClassroom();
 
 });
 
@@ -39,21 +35,16 @@ const classroomsResults = (data) => {
   $('#results').append('<table id="classrooms_index"><tbody><tr><th>Class Name</th><th>Teacher</th><th>Age Range</th><th>Number of Students</th></tr></tbody></table>')
 
 
-  // sort the classroom list
+  //sort the classroom list
   $('#sort-button').on('click', function(e){
-
     data = sortTheData(data)
-    console.log(data)
     $('#classrooms_index').empty()
     $('#classrooms_index').append('<table id="classrooms_index"><tbody><tr><th>Class Name</th><th>Teacher</th><th>Age Range</th><th>Number of Students</th></tr></tbody></table>')
-
     populateClassroomList(data);
-
   })
 
   // takes the data and fills the #results with the list of clssrooms
   populateClassroomList(data);
-
 
   loadClassroom();
   loadClassroomForm();
@@ -65,8 +56,8 @@ const parentsResults = (data) => {
 
   data.forEach((parent) => {
     console.log(parent)
-    let parent_name = parent.first_name + ' ' + parent.last_name
-    let baseURL = 'https://localhost:3000/parents/'
+    const parent_name = parent.first_name + ' ' + parent.last_name
+    const baseURL = 'https://localhost:3000/parents/'
     $('#results').append('<li><a href='+ baseURL + parent.id + '>' + parent_name + '</a></li>')
   });
 
@@ -79,16 +70,13 @@ const childrenResults = (data) => {
   $('#results').append('<table id="children_index"><tbody><tr><td>Child Name</td><td>Age</td><td>Edit Column</td><td>Delete Column</td></tr></tbody></table>')
 
   data.forEach((child) => {
-    console.log(child)
-    let child_name = child.first_name + ' ' + child.last_name
-    let age = Date.now() - timeStamp(child.dob);
-    console.log(age)
+    const child_name = child.first_name + ' ' + child.last_name
+    const age = Date.now() - timeStamp(child.dob);
     const baseURL = 'https://localhost:3000/children/'
-    let editURL = '<a href="https://localhost:3000/children/' + child.id + '/edit">Edit</a>'
-    let deleteURL = '<a data-confirm="Confirm Deletion" rel="nofollow" data-method="delete" href="https://localhost:3000/children/' + child.id + '">Delete</a>'
+    const editURL = '<a href="https://localhost:3000/children/' + child.id + '/edit">Edit</a>'
+    const deleteURL = '<a data-confirm="Confirm Deletion" rel="nofollow" data-method="delete" href="https://localhost:3000/children/' + child.id + '">Delete</a>'
     // '<a data-confirm="Confirm Deletion" rel="nofollow" data-method="delete" href="/children/3">Delete</a>'
 
-    console.log(deleteURL)
     $('#children_index').append('<tr><td><a href='+ baseURL + child.id + '>' + child_name + '</a></td><td>' + getChildAge(child.dob) + '</td><td>' + editURL + '</td><td>' + deleteURL + '</td></tr>')
   });
   loadChild();
@@ -99,29 +87,21 @@ const childrenResults = (data) => {
 
 // changes string date to timestamp
 const timeStamp = (stringDate) => {
-  let datum = Date.parse(stringDate);
+  const datum = Date.parse(stringDate);
   return datum;
 }
 //converts to human readable time format
 const timeConverter = (UNIX_timestamp)=> {
-  let time = new Date(UNIX_timestamp);
+  const time = new Date(UNIX_timestamp);
   const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  let year = time.getFullYear();
-  let month = months[time.getMonth()];
-  let date = time.getDate();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let sec = time.getSeconds();
-  let convertedTime = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  const year = time.getFullYear();
+  const month = months[time.getMonth()];
+  const date = time.getDate();
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  const sec = time.getSeconds();
+  const convertedTime = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
   return convertedTime;
-}
-
-
-const sortClassroom = function(data) {
-  $('#sort-button').on('click', function(e){
-    console.log(this)
-  })
-  console.log(data)
 }
 
 const sortTheData = function(data) {
@@ -134,7 +114,6 @@ const sortTheData = function(data) {
       if (nameA > nameB) {
         return 1;
       }
-
       // names must be equal
       return 0;
   });
@@ -142,7 +121,6 @@ const sortTheData = function(data) {
 }
 
 const populateClassroomList = function(data) {
-
 
   data.forEach((classroom) => {
     const baseURL = 'https://localhost:3000/admin/classrooms/'
